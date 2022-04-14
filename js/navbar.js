87cbb7397232ddd5c0e4d0a81f73b8eb89b504d0
Llona André--Augustine <llona.andre--augustine@unice.fr>
2022-04-14 20:07:38 +0200

    Debut des skills

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+document.write(` <nav>
+
+<p><a href=".">Portefolio</a></p>
+
+<ul>
+    <li><a href="">À propos</a></li>
+    <li><a href="">Projet</a></li>
+    <li><a href="skill.html">CV</a></li>
+    <li><a href="https://www.linkedin.com/in/llona-andr%C3%A9-augustine-26aa64222/">Linkedin</a></li>
+</ul>
+
+</nav>`);
